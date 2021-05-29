ce00192ef5a208bc81946770b9718175664c55e7
takuya ichikawa <rv0034pv@kankyousystem.jp>
2021-05-30 00:44:41 +0900

    initialize git

---
+++ test.rb
@@ -0,0 +1,13 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'kadai.html.erb')
+server.mount('/kadai1.cgi', WEBrick::HTTPServlet::CGIHandler, 'kadai1.rb')
+server.mount('/kadai2.cgi', WEBrick::HTTPServlet::CGIHandler, 'kadai2.rb')
+server.start
